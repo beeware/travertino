f6eaeb04d0783708446e6abde58052775d9d6f51
Charles Whittington <charleswhitt@gmail.com>
2024-02-27 19:40:24 -0500

    Updated to tests to pytest, and from 12-dev to 12 in CI.

--- tests/test_choices.py
+++ tests/test_choices.py
@@ -2,8 +2,9 @@ from __future__ import annotations
 
 import sys
 from dataclasses import dataclass
-from unittest import TestCase
 from unittest.mock import Mock
+
+import pytest
 
 from travertino.colors import NAMED_COLOR, rgb
 from travertino.constants import GOLDENROD, NONE, REBECCAPURPLE, TOP
@@ -27,374 +28,346 @@ def prep_style_class(cls):
     return dataclass(**_DATACLASS_KWARGS)(cls)
 
 
-class PropertyChoiceTests(TestCase):
-    def assert_property(self, obj, value, check_mock=True):
-        self.assertEqual(obj.prop, value)
-        if check_mock:
-            obj.apply.assert_called_once_with("prop", value)
-            obj.apply.reset_mock()
+def test_none():
+    @prep_style_class
+    class MyObject(BaseStyle):
+        prop: str = validated_property(
+            choices=Choices(NONE, REBECCAPURPLE), initial=NONE
+        )
 
-    def test_none(self):
-        @prep_style_class
-        class MyObject(BaseStyle):
-            prop: str = validated_property(
-                choices=Choices(NONE, REBECCAPURPLE), initial=NONE
-            )
+    obj = MyObject()
+    assert obj.prop == NONE
 
-        obj = MyObject()
-        self.assert_property(obj, NONE, check_mock=False)
+    with pytest.raises(ValueError):
+        obj.prop = 10
 
-        with self.assertRaises(ValueError):
-            obj.prop = 10
+    with pytest.raises(ValueError):
+        obj.prop = 3.14159
 
-        with self.assertRaises(ValueError):
-            obj.prop = 3.14159
-
-        with self.assertRaises(ValueError):
-            obj.prop = "#112233"
-
-        with self.assertRaises(ValueError):
-            obj.prop = "a"
-
-        with self.assertRaises(ValueError):
-            obj.prop = "b"
-
-        # Set the property to a different explicit value
-        obj.prop = REBECCAPURPLE
-        self.assert_property(obj, REBECCAPURPLE)
-
-        # A Travertino NONE is an explicit value
-        obj.prop = NONE
-        self.assert_property(obj, NONE)
-
-        # Set the property to a different explicit value
-        obj.prop = REBECCAPURPLE
-        self.assert_property(obj, REBECCAPURPLE)
-
-        # A Python None is invalid
-        with self.assertRaises(ValueError):
-            obj.prop = None
-
-        # The property can be reset
-        del obj.prop
-        self.assert_property(obj, NONE)
-
-        # Check the error message
-        try:
-            obj.prop = "invalid"
-            self.fail("Should raise ValueError")
-        except ValueError as v:
-            self.assertEqual(
-                str(v),
-                "Invalid value 'invalid' for property prop; Valid values are: none, rebeccapurple",
-            )
-
-    def test_allow_string(self):
-        @prep_style_class
-        class MyObject(BaseStyle):
-            prop: str = validated_property(
-                choices=Choices(string=True), initial="start"
-            )
-
-        obj = MyObject()
-        self.assertEqual(obj.prop, "start")
-
-        with self.assertRaises(ValueError):
-            obj.prop = 10
-
-        with self.assertRaises(ValueError):
-            obj.prop = 3.14159
-
-        obj.prop = REBECCAPURPLE
-        self.assert_property(obj, "rebeccapurple")
-
+    with pytest.raises(ValueError):
         obj.prop = "#112233"
-        self.assert_property(obj, "#112233")
 
+    with pytest.raises(ValueError):
         obj.prop = "a"
-        self.assert_property(obj, "a")
 
+    with pytest.raises(ValueError):
         obj.prop = "b"
-        self.assert_property(obj, "b")
 
-        # A Travertino NONE is an explicit string value
-        obj.prop = NONE
-        self.assert_property(obj, NONE)
+    # Set the property to a different explicit value
+    obj.prop = REBECCAPURPLE
+    assert obj.prop == REBECCAPURPLE
 
-        # A Python None is invalid
-        with self.assertRaises(ValueError):
-            obj.prop = None
+    # A Travertino NONE is an explicit value
+    obj.prop = NONE
+    assert obj.prop == NONE
 
-        # The property can be reset
-        del obj.prop
-        self.assert_property(obj, "start")
+    # Set the property to a different explicit value
+    obj.prop = REBECCAPURPLE
+    assert obj.prop == REBECCAPURPLE
 
-        # Check the error message
-        try:
-            obj.prop = 99
-            self.fail("Should raise ValueError")
-        except ValueError as v:
-            self.assertEqual(
-                str(v), "Invalid value 99 for property prop; Valid values are: <string>"
-            )
+    # A Python None is invalid
+    with pytest.raises(ValueError):
+        obj.prop = None
 
-    def test_allow_integer(self):
-        @prep_style_class
-        class MyObject(BaseStyle):
-            prop: int = validated_property(choices=Choices(integer=True), initial=0)
+    # The property can be reset
+    del obj.prop
+    assert obj.prop == NONE
 
-        obj = MyObject()
-        self.assertEqual(obj.prop, 0)
+    with pytest.raises(
+        ValueError,
+        match=r"Invalid value 'invalid' for property prop; Valid values are: "
+        r"none, rebeccapurple",
+    ):
+        obj.prop = "invalid"
 
+
+def test_allow_string():
+    @prep_style_class
+    class MyObject(BaseStyle):
+        prop: str = validated_property(choices=Choices(string=True), initial="start")
+
+    obj = MyObject()
+    assert obj.prop == "start"
+
+    with pytest.raises(ValueError):
         obj.prop = 10
-        self.assert_property(obj, 10)
 
-        # This is an odd case; Python happily rounds floats to integers.
-        # It's more trouble than it's worth to correct this.
+    with pytest.raises(ValueError):
         obj.prop = 3.14159
-        self.assert_property(obj, 3)
 
-        with self.assertRaises(ValueError):
-            obj.prop = REBECCAPURPLE
+    obj.prop = REBECCAPURPLE
+    assert obj.prop == "rebeccapurple"
 
-        with self.assertRaises(ValueError):
-            obj.prop = "#112233"
+    obj.prop = "#112233"
+    assert obj.prop == "#112233"
 
-        with self.assertRaises(ValueError):
-            obj.prop = "a"
+    obj.prop = "a"
+    assert obj.prop == "a"
 
-        with self.assertRaises(ValueError):
-            obj.prop = "b"
+    obj.prop = "b"
+    assert obj.prop == "b"
 
-        # A Travertino NONE is an explicit string value
-        with self.assertRaises(ValueError):
-            obj.prop = NONE
+    # A Travertino NONE is an explicit string value
+    obj.prop = NONE
+    assert obj.prop == NONE
 
-        # A Python None is invalid
-        with self.assertRaises(ValueError):
-            obj.prop = None
+    # A Python None is invalid
+    with pytest.raises(ValueError):
+        obj.prop = None
 
-        # The property can be reset
-        del obj.prop
-        self.assert_property(obj, 0)
+    # The property can be reset
+    del obj.prop
+    assert obj.prop == "start"
 
-        # Check the error message
-        try:
-            obj.prop = "invalid"
-            self.fail("Should raise ValueError")
-        except ValueError as v:
-            self.assertEqual(
-                str(v),
-                "Invalid value 'invalid' for property prop; Valid values are: <integer>",
-            )
+    with pytest.raises(
+        ValueError,
+        match=r"Invalid value 99 for property prop; Valid values are: <string>",
+    ):
+        obj.prop = 99
 
-    def test_allow_number(self):
-        @prep_style_class
-        class MyObject(BaseStyle):
-            prop: float = validated_property(choices=Choices(number=True), initial=0)
 
-        obj = MyObject()
-        self.assertEqual(obj.prop, 0)
+def test_allow_integer():
+    @prep_style_class
+    class MyObject(BaseStyle):
+        prop: int = validated_property(choices=Choices(integer=True), initial=0)
 
-        obj.prop = 10
-        self.assert_property(obj, 10.0)
+    obj = MyObject()
+    assert obj.prop == 0
 
-        obj.prop = 3.14159
-        self.assert_property(obj, 3.14159)
+    obj.prop = 10
+    assert obj.prop == 10
 
-        with self.assertRaises(ValueError):
-            obj.prop = REBECCAPURPLE
+    # This is an odd case; Python happily rounds floats to integers.
+    # It's more trouble than it's worth to correct this.
+    obj.prop = 3.14159
+    assert obj.prop == 3
 
-        with self.assertRaises(ValueError):
-            obj.prop = "#112233"
-
-        with self.assertRaises(ValueError):
-            obj.prop = "a"
-
-        with self.assertRaises(ValueError):
-            obj.prop = "b"
-
-        # A Travertino NONE is an explicit string value
-        with self.assertRaises(ValueError):
-            obj.prop = NONE
-
-        # A Python None is invalid
-        with self.assertRaises(ValueError):
-            obj.prop = None
-
-        # The property can be reset
-        del obj.prop
-        self.assert_property(obj, 0)
-
-        # Check the error message
-        try:
-            obj.prop = "invalid"
-            self.fail("Should raise ValueError")
-        except ValueError as v:
-            self.assertEqual(
-                str(v),
-                "Invalid value 'invalid' for property prop; Valid values are: <number>",
-            )
-
-    def test_allow_color(self):
-        @prep_style_class
-        class MyObject(BaseStyle):
-            prop: str = validated_property(
-                choices=Choices(color=True), initial="goldenrod"
-            )
-
-        obj = MyObject()
-        self.assertEqual(obj.prop, NAMED_COLOR[GOLDENROD])
-
-        with self.assertRaises(ValueError):
-            obj.prop = 10
-
-        with self.assertRaises(ValueError):
-            obj.prop = 3.14159
-
+    with pytest.raises(ValueError):
         obj.prop = REBECCAPURPLE
-        self.assert_property(obj, NAMED_COLOR[REBECCAPURPLE])
 
+    with pytest.raises(ValueError):
         obj.prop = "#112233"
-        self.assert_property(obj, rgb(0x11, 0x22, 0x33))
 
-        with self.assertRaises(ValueError):
-            obj.prop = "a"
-
-        with self.assertRaises(ValueError):
-            obj.prop = "b"
-
-        # A Travertino NONE is an explicit string value
-        with self.assertRaises(ValueError):
-            obj.prop = NONE
-
-        # A Python None is invalid
-        with self.assertRaises(ValueError):
-            obj.prop = None
-
-        # The property can be reset
-        del obj.prop
-        self.assert_property(obj, NAMED_COLOR["goldenrod"])
-
-        # Check the error message
-        try:
-            obj.prop = "invalid"
-            self.fail("Should raise ValueError")
-        except ValueError as v:
-            self.assertEqual(
-                str(v),
-                "Invalid value 'invalid' for property prop; Valid values are: <color>",
-            )
-
-    def test_values(self):
-        @prep_style_class
-        class MyObject(BaseStyle):
-            prop: str = validated_property(choices=Choices("a", "b", NONE), initial="a")
-
-        obj = MyObject()
-        self.assertEqual(obj.prop, "a")
-
-        with self.assertRaises(ValueError):
-            obj.prop = 10
-
-        with self.assertRaises(ValueError):
-            obj.prop = 3.14159
-
-        with self.assertRaises(ValueError):
-            obj.prop = REBECCAPURPLE
-
-        with self.assertRaises(ValueError):
-            obj.prop = "#112233"
-
-        obj.prop = NONE
-        self.assert_property(obj, NONE)
-
-        obj.prop = "b"
-        self.assert_property(obj, "b")
-
-        # A Python None is invalid
-        with self.assertRaises(ValueError):
-            obj.prop = None
-
-        # The property can be reset
-        del obj.prop
-        self.assert_property(obj, "a")
-
-        # Check the error message
-        try:
-            obj.prop = "invalid"
-            self.fail("Should raise ValueError")
-        except ValueError as v:
-            self.assertEqual(
-                str(v),
-                "Invalid value 'invalid' for property prop; Valid values are: a, b, none",
-            )
-
-    def test_multiple_choices(self):
-        @prep_style_class
-        class MyObject(BaseStyle):
-            prop: str | float = validated_property(
-                choices=Choices("a", "b", NONE, number=True, color=True),
-                initial=None,
-            )
-
-        obj = MyObject()
-
-        obj.prop = 10
-        self.assert_property(obj, 10.0)
-
-        obj.prop = 3.14159
-        self.assert_property(obj, 3.14159)
-
-        obj.prop = REBECCAPURPLE
-        self.assert_property(obj, NAMED_COLOR[REBECCAPURPLE])
-
-        obj.prop = "#112233"
-        self.assert_property(obj, rgb(0x11, 0x22, 0x33))
-
+    with pytest.raises(ValueError):
         obj.prop = "a"
-        self.assert_property(obj, "a")
 
-        obj.prop = NONE
-        self.assert_property(obj, NONE)
-
+    with pytest.raises(ValueError):
         obj.prop = "b"
-        self.assert_property(obj, "b")
 
-        # A Python None is invalid
-        with self.assertRaises(ValueError):
-            obj.prop = None
+    # A Travertino NONE is an explicit string value
+    with pytest.raises(ValueError):
+        obj.prop = NONE
 
-        # The property can be reset
-        # There's no initial value, so the property is None
-        del obj.prop
-        self.assertIsNone(obj.prop)
+    # A Python None is invalid
+    with pytest.raises(ValueError):
+        obj.prop = None
 
-        # Check the error message
-        try:
-            obj.prop = "invalid"
-            self.fail("Should raise ValueError")
-        except ValueError as v:
-            self.assertEqual(
-                str(v),
-                "Invalid value 'invalid' for property prop; "
-                "Valid values are: a, b, none, <number>, <color>",
-            )
+    # The property can be reset
+    del obj.prop
+    assert obj.prop == 0
 
-    def test_string_symbol(self):
-        @prep_style_class
-        class MyObject(BaseStyle):
-            prop: str = validated_property(choices=Choices(TOP, NONE))
+    # Check the error message
+    with pytest.raises(
+        ValueError,
+        match=r"Invalid value 'invalid' for property prop; Valid values are: <integer>",
+    ):
+        obj.prop = "invalid"
 
-        obj = MyObject()
 
-        # Set a symbolic value using the string value of the symbol
-        # We can't just use the string directly, though - that would
-        # get optimized by the compiler. So we create a string and
-        # transform it into the value we want.
-        val = "TOP"
-        obj.prop = val.lower()
+def test_allow_number():
+    @prep_style_class
+    class MyObject(BaseStyle):
+        prop: float = validated_property(choices=Choices(number=True), initial=0)
 
-        # Both equality and instance checking should work.
-        self.assertEqual(obj.prop, TOP)
-        self.assertIs(obj.prop, TOP)
+    obj = MyObject()
+    assert obj.prop == 0
+
+    obj.prop = 10
+    assert obj.prop == 10.0
+
+    obj.prop = 3.14159
+    assert obj.prop == 3.14159
+
+    with pytest.raises(ValueError):
+        obj.prop = REBECCAPURPLE
+
+    with pytest.raises(ValueError):
+        obj.prop = "#112233"
+
+    with pytest.raises(ValueError):
+        obj.prop = "a"
+
+    with pytest.raises(ValueError):
+        obj.prop = "b"
+
+    # A Travertino NONE is an explicit string value
+    with pytest.raises(ValueError):
+        obj.prop = NONE
+
+    # A Python None is invalid
+    with pytest.raises(ValueError):
+        obj.prop = None
+
+    # The property can be reset
+    del obj.prop
+    assert obj.prop == 0
+
+    with pytest.raises(
+        ValueError,
+        match=r"Invalid value 'invalid' for property prop; Valid values are: <number>",
+    ):
+        obj.prop = "invalid"
+
+
+def test_allow_color():
+    @prep_style_class
+    class MyObject(BaseStyle):
+        prop: str = validated_property(choices=Choices(color=True), initial="goldenrod")
+
+    obj = MyObject()
+    assert obj.prop == NAMED_COLOR[GOLDENROD]
+
+    with pytest.raises(ValueError):
+        obj.prop = 10
+
+    with pytest.raises(ValueError):
+        obj.prop = 3.14159
+
+    obj.prop = REBECCAPURPLE
+    assert obj.prop == NAMED_COLOR[REBECCAPURPLE]
+
+    obj.prop = "#112233"
+    assert obj.prop == rgb(0x11, 0x22, 0x33)
+
+    with pytest.raises(ValueError):
+        obj.prop = "a"
+
+    with pytest.raises(ValueError):
+        obj.prop = "b"
+
+    # A Travertino NONE is an explicit string value
+    with pytest.raises(ValueError):
+        obj.prop = NONE
+
+    # A Python None is invalid
+    with pytest.raises(ValueError):
+        obj.prop = None
+
+    # The property can be reset
+    del obj.prop
+    assert obj.prop == NAMED_COLOR["goldenrod"]
+
+    with pytest.raises(
+        ValueError,
+        match=r"Invalid value 'invalid' for property prop; Valid values are: <color>",
+    ):
+        obj.prop = "invalid"
+
+
+def test_values():
+    @prep_style_class
+    class MyObject(BaseStyle):
+        prop: str = validated_property(choices=Choices("a", "b", NONE), initial="a")
+
+    obj = MyObject()
+    assert obj.prop == "a"
+
+    with pytest.raises(ValueError):
+        obj.prop = 10
+
+    with pytest.raises(ValueError):
+        obj.prop = 3.14159
+
+    with pytest.raises(ValueError):
+        obj.prop = REBECCAPURPLE
+
+    with pytest.raises(ValueError):
+        obj.prop = "#112233"
+
+    obj.prop = NONE
+    assert obj.prop == NONE
+
+    obj.prop = "b"
+    assert obj.prop == "b"
+
+    # A Python None is invalid
+    with pytest.raises(ValueError):
+        obj.prop = None
+
+    # The property can be reset
+    del obj.prop
+    assert obj.prop == "a"
+
+    with pytest.raises(
+        ValueError,
+        match=r"Invalid value 'invalid' for property prop; Valid values are: a, b, none",
+    ):
+        obj.prop = "invalid"
+
+
+def test_multiple_choices():
+    @prep_style_class
+    class MyObject(BaseStyle):
+        prop: str | float = validated_property(
+            choices=Choices("a", "b", NONE, number=True, color=True),
+            initial=None,
+        )
+
+    obj = MyObject()
+
+    obj.prop = 10
+    assert obj.prop == 10.0
+
+    obj.prop = 3.14159
+    assert obj.prop == 3.14159
+
+    obj.prop = REBECCAPURPLE
+    assert obj.prop == NAMED_COLOR[REBECCAPURPLE]
+
+    obj.prop = "#112233"
+    assert obj.prop == rgb(0x11, 0x22, 0x33)
+
+    obj.prop = "a"
+    assert obj.prop == "a"
+
+    obj.prop = NONE
+    assert obj.prop == NONE
+
+    obj.prop = "b"
+    assert obj.prop == "b"
+
+    # A Python None is invalid
+    with pytest.raises(ValueError):
+        obj.prop = None
+
+    # The property can be reset
+    # There's no initial value, so the property is None
+    del obj.prop
+    assert obj.prop is None
+
+    # Check the error message
+    with pytest.raises(
+        ValueError,
+        match=r"Invalid value 'invalid' for property prop; Valid values are: "
+        r"a, b, none, <number>, <color>",
+    ):
+        obj.prop = "invalid"
+
+
+def test_string_symbol():
+    @prep_style_class
+    class MyObject(BaseStyle):
+        prop: str = validated_property(choices=Choices(TOP, NONE))
+
+    obj = MyObject()
+
+    # Set a symbolic value using the string value of the symbol
+    # We can't just use the string directly, though - that would
+    # get optimized by the compiler. So we create a string and
+    # transform it into the value we want.
+    val = "TOP"
+    obj.prop = val.lower()
+
+    # Both equality and instance checking should work.
+    assert obj.prop == TOP
+    assert obj.prop is TOP
