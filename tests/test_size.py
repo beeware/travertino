7fa29780335d76a8e90c700db988ee4c8b17bfe1
Charles Whittington <charleswhitt@gmail.com>
2024-03-10 22:56:29 -0400

    Separated absolute and minimum size back into separate tests

--- tests/test_size.py
+++ tests/test_size.py
@@ -52,43 +52,65 @@ def test_size_repr(box):
 
 
 @pytest.mark.parametrize("dimension", ["width", "height"])
-@pytest.mark.parametrize("minimum", [True, False])
-def test_set_dimension(box, dimension, minimum):
-    dim = at_least if minimum else lambda x: x
-
-    setattr(box.size, dimension, dim(10))
-    assert_size(box.size, BASE_SIZE.change(dimension, dim(10)))
+def test_set_dimension(box, dimension):
+    setattr(box.size, dimension, 10)
+    assert_size(box.size, BASE_SIZE.change(dimension, 10))
 
     # Layout has been dirtied.
-    box.layout.dirty.assert_called_once_with(**{f"intrinsic_{dimension}": dim(10)})
+    box.layout.dirty.assert_called_once_with(**{f"intrinsic_{dimension}": 10})
 
     # Clean the layout
     box.layout.dirty.reset_mock()
 
     # Set the width to the same value
-    setattr(box.size, dimension, dim(10))
-    assert_size(box.size, BASE_SIZE.change(dimension, dim(10)))
+    setattr(box.size, dimension, 10)
+    assert_size(box.size, BASE_SIZE.change(dimension, 10))
 
     # Layout has NOT been dirtied.
     box.layout.dirty.assert_not_called()
 
-    if minimum:
-        # Set the width to the same value, but not as a minimum
-        setattr(box.size, dimension, 10)
-        assert_size(box.size, BASE_SIZE.change(dimension, 10))
-
-        # Layout has been dirtied.
-        box.layout.dirty.assert_called_once_with(**{f"intrinsic_{dimension}": 10})
-
-        # Clean the layout
-        box.layout.dirty.reset_mock()
-
     # Set the width to something new
-    setattr(box.size, dimension, dim(20))
-    assert_size(box.size, BASE_SIZE.change(dimension, dim(20)))
+    setattr(box.size, dimension, 20)
+    assert_size(box.size, BASE_SIZE.change(dimension, 20))
 
     # Layout has been dirtied.
-    box.layout.dirty.assert_called_once_with(**{f"intrinsic_{dimension}": dim(20)})
+    box.layout.dirty.assert_called_once_with(**{f"intrinsic_{dimension}": 20})
+
+
+@pytest.mark.parametrize("dimension", ["width", "height"])
+def test_set_dimension_at_least(box, dimension):
+    setattr(box.size, dimension, at_least(10))
+    assert_size(box.size, BASE_SIZE.change(dimension, at_least(10)))
+
+    # Layout has been dirtied.
+    box.layout.dirty.assert_called_once_with(**{f"intrinsic_{dimension}": at_least(10)})
+
+    # Clean the layout
+    box.layout.dirty.reset_mock()
+
+    # Set the width to the same value
+    setattr(box.size, dimension, at_least(10))
+    assert_size(box.size, BASE_SIZE.change(dimension, at_least(10)))
+
+    # Layout has NOT been dirtied.
+    box.layout.dirty.assert_not_called()
+
+    # Set the width to the same value, but not as a minimum
+    setattr(box.size, dimension, 10)
+    assert_size(box.size, BASE_SIZE.change(dimension, 10))
+
+    # Layout has been dirtied.
+    box.layout.dirty.assert_called_once_with(**{f"intrinsic_{dimension}": 10})
+
+    # Clean the layout
+    box.layout.dirty.reset_mock()
+
+    # Set the width to something new
+    setattr(box.size, dimension, at_least(20))
+    assert_size(box.size, BASE_SIZE.change(dimension, at_least(20)))
+
+    # Layout has been dirtied.
+    box.layout.dirty.assert_called_once_with(**{f"intrinsic_{dimension}": at_least(20)})
 
 
 def test_set_ratio(box):
